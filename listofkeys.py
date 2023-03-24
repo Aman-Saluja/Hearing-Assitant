6ed023f7a1a38491fdc387d57ee91bb22453d548
Aman-Saluja <59624250+Aman-Saluja@users.noreply.github.com>
2023-03-23 20:25:25 -0400

    Add list of keys and favicon

---
+++ listofkeys.py
@@ -0,0 +1,34 @@
+replace_list = {
+    'alpha': 'α',
+    'beta': 'β',
+    'gamma': 'γ',
+    'lambda': 'λ',
+    'plus': '+',
+    'add': '+',
+    'subtract': '-',
+    'minus': '-',
+    'into': '*',
+    'multiply': 'X',
+    'multiplied by': 'X',
+    'divide': '/',
+    'divided by': '/',
+    'by': '/',
+    'percentage': '%',
+    'mod': '%',
+    'theta': 'θ',
+    'integration': '∫',
+    'Integration of': '∫',
+    'integration of': '∫',
+    'derivative': '∂',
+    'differentiation': '∂',
+    'equal to': '=',
+    'equals to': '=',
+    'equas': '=',
+    'summation':'Σ',
+    'delta': 'Δ',
+    'of': ''
+}
+
+
+def get():
+    return symbol
